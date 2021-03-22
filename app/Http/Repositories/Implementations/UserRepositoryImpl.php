35b195e2f4c46dd90d0acddfd750877988f8295c
Matheus Caetano <caetanomatheus876@gmail.com>
2021-03-22 20:33:43 -0300

    Finished user creation endpoint

--- app/Http/Repositories/Implementations/UserRepositoryImpl.php
+++ app/Http/Repositories/Implementations/UserRepositoryImpl.php
@@ -3,13 +3,13 @@
 namespace App\Http\Repositories\Implementations;
 
 use App\Http\Repositories\UserRepository;
-use App\Models\Post;
+use App\Models\User;
 
 class UserRepositoryImpl implements UserRepository
 {
 
   public function create(array $data): object
   {
-    return Post::create($data);
+    return User::create($data);
   }
 }
