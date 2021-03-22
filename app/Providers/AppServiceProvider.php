7c9ef35925facdc9aeb6721ae341499b7446bb73
Matheus Caetano <caetanomatheus876@gmail.com>
2021-03-22 20:41:26 -0300

    Added dependency injection configuration for comment repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use App\Http\Repositories\Implementations\PostRepositoryImpl;
 use App\Http\Repositories\Implementations\UserRepositoryImpl;
+use App\Http\Repositories\Implementations\CommentRepositoryImpl;
 use App\Http\Repositories\PostRepository;
 use App\Http\Repositories\UserRepository;
+use App\Http\Repositories\CommentRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->singleton(PostRepository::class, fn() => new PostRepositoryImpl());
         $this->app->singleton(UserRepository::class, fn() => new UserRepositoryImpl());
+        $this->app->singleton(CommentRepository::class, fn() => new CommentRepositoryImpl());
     }
 
     public function boot(): void
